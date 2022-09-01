bf76092cc15f9da9f63b68d688166db2154fed5a
Seth Franklin <sethfranklin@Seths-MacBook-Air.local>
2022-09-01 05:37:24 -0400

    Wrote SQL commands to intialize page table

--- server/db.js
+++ server/db.js
@@ -1,7 +1,45 @@
 
+/*
+API calls:
+/page/ POST - needs body and next page id
+- returns new page id
+/page/ GET - needs page id
+- returns object type {page id, body, next page id, timestamp, num pages before, num pages left}
+/page/ PATCH? - needs page id and new body
+- returns page id
+/page/ DELETE - needs page id
+- returns next page id
+/leaves/ GET (dump leaves sorted by latest timestamp) - needs no arguments
+- returns array of objects type {page id, preview, num pages left}
+*/
+
 import 'dotenv/config';
 import pg from 'pg';
 const { Pool } = pg;
+
+const createPageTableStatement = `
+	create table if not exists page (
+		pid integer primary key,
+		body varchar(2048),
+		preview varchar(30),
+		next_pid integer,
+		timestamp bigint,
+		num_prior integer,
+		num_left integer
+	);
+`;
+
+const insertLastPageStatement = `
+	insert into page values (
+		0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis lectus magna fringilla urna. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Ut etiam sit amet nisl purus in mollis nunc. Vulputate mi sit amet mauris commodo. Aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod. Sed odio morbi quis commodo odio aenean. Felis bibendum ut tristique et egestas. Amet justo donec enim diam vulputate ut pharetra sit amet. Nibh venenatis cras sed felis eget velit aliquet sagittis. Id cursus metus aliquam eleifend mi. Egestas fringilla phasellus faucibus scelerisque eleifend. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Arcu dictum varius duis at consectetur lorem donec massa sapien. Viverra justo nec ultrices dui sapien. Sed cras ornare arcu dui vivamus arcu felis bibendum. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est.',
+		'Lorem ipsum dolor si...',
+		NULL,
+		${Date.now()},
+		0,
+		0
+	) on conflict (pid) do nothing;
+`;
 
 class BackwordsDB {
 
@@ -9,6 +47,7 @@ class BackwordsDB {
 	}
 
 	async initialize() {
+
 		const pool = new Pool({
 			connectionString: process.env.DATABASE_URL,
 			ssl: { rejectUnauthorized: false }
@@ -16,7 +55,10 @@ class BackwordsDB {
 
 		this.client = await pool.connect();
 
-		// Do database page table initialize command(s)
+		// Do database page table initialize commands
+		await this.client.query(createPageTableStatement);
+		await this.client.query(insertLastPageStatement);
+
 	}
 
 	async createPage(body, next_pid) {
