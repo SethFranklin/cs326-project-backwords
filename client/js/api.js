51e5e85d835d1673e54d97c1826d16b348dd3ebf
Seth Franklin <sethfranklin@Seths-MacBook-Air.local>
2022-09-01 03:52:52 -0400

    Removed unecessary comments from api.js

--- client/js/api.js
+++ client/js/api.js
@@ -1,20 +1,4 @@
 
-/*
-/page/ POST - needs body and next page id
-- returns new page id
-/page/ GET - needs page id
-- returns object type {page id, body, next page id, timestamp, num pages before, num pages left}
-/page/ PATCH? - needs page id and new body
-- returns page id
-/page/ DELETE - needs page id
-- returns next page id
-/leaves/ GET (dump leaves sorted by latest timestamp) - needs no arguments
-- returns array of objects type {page id, preview, num pages left}
-*/
-
-// @TODO: make actual api calls using fetch to the server isntead of making dummy calls
-// @TODO: write function signatures/explanations. are comments required for this project?
-
 async function createPage(body, next_pid) {
 	const response = await fetch("/page?" + new URLSearchParams({
 		body: body,
